b1cf9c850c6d34f8a72ff4099fdbfbd6e5adc79b
Damiano <htutwaiphyoe@gmail.com>
2020-08-26 23:01:56 +0700

    feat: theme setup

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,10 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl">React Native Expo</Text>
+    <View className="flex-1 items-center justify-center ">
+      <Text className="text-lg text-primary p-screen-horizontal">
+        React Native Expo
+      </Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import { colors, fontSize, screenPadding } from "./styles/tokens";
+
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      fontSize,
+      spacing: {
+        "screen-horizontal": screenPadding.horizontal,
+      },
+    },
   },
   plugins: [],
 };
