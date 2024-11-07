4824b0be4bbd39603dd0b020e02b7d23604c001c
Damiano <htutwaiphyoe@gmail.com>
2024-11-07 23:55:04 +0700

    feat: utility classes

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="items-center justify-center flex-1 ">
+      <Text className="text-lg text-primary p-screen-horizontal slider">
+        React Native Expo
+      </Text>
+      <Text className="empty">d</Text>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,21 @@
 import { Stack } from "expo-router";
 import "@/styles/global.css";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Layout() {
   return (
+    <SafeAreaProvider>
+      <RootNavigation />
+      <StatusBar style="auto" />
+    </SafeAreaProvider>
+  );
+}
+
+function RootNavigation() {
+  return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
     </Stack>
   );
 }
