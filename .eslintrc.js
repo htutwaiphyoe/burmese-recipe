0dcb029585a533f7ef7012d7c8070f90f9ab0375
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-11-24 23:19:28 +0700

    feat: repo reset

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier", "react-native"],
-  rules: {
-    "prettier/prettier": "no",
-    "react-native/no-unused-styles": "error",
-  },
-};
--- utils/index.ts
+++
@@ -1,4 +0,0 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export const cn = (...classes: ClassValue[]) => twMerge(clsx(classes));
