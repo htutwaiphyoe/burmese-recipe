853da30952e0fd72e8dcdab2757f17e3e6cd10b7
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-11-25 23:56:34 +0700

    feat: google login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,8 +12,37 @@ import {
   Outfit_800ExtraBold,
   Outfit_900Black,
 } from "@expo-google-fonts/outfit";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
 
 export default function Layout() {
+  const tokenCache = {
+    async getToken(key: string) {
+      try {
+        const item = await SecureStore.getItemAsync(key);
+        if (item) {
+          console.log(`${key} was used 🔐 \n`);
+        } else {
+          console.log("No values stored under key: " + key);
+        }
+        return item;
+      } catch (error) {
+        console.error("SecureStore get item error: ", error);
+        await SecureStore.deleteItemAsync(key);
+        return null;
+      }
+    },
+    async saveToken(key: string, value: string) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (err) {
+        return;
+      }
+    },
+  };
+
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
   const [isFontsLoaded] = useFonts({
     Outfit_100Thin,
     Outfit_200ExtraLight,
@@ -29,19 +58,23 @@ export default function Layout() {
   if (!isFontsLoaded) return null;
 
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: "Pethub",
-        }}
-      />
-      <Stack.Screen
-        name="login"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Pethub",
+            }}
+          />
+          <Stack.Screen
+            name="login"
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
--- app/login.tsx
+++ app/login.tsx
@@ -1,28 +1,55 @@
-import { Link } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking";
+import { useCallback, useEffect } from "react";
+
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    void WebBrowser.warmUpAsync();
+    return () => {
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = useCallback(async () => {
+    try {
+      const { createdSessionId, setActive } = await startOAuthFlow({
+        redirectUrl: Linking.createURL("/", { scheme: "pethub" }),
+      });
+
+      if (createdSessionId) setActive!({ session: createdSessionId });
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
     <View className="flex-1">
       <Image
         className="w-full h-1/2"
         source={require("assets/images/login.png")}
       />
-      <View className="flex-1 justify-evenly px-10 gap-5">
-        <View className="justify-center items-center gap-5">
-          <Text className="text-center font-bold text-4xl">
+      <View className="flex-1 gap-5 px-10 justify-evenly">
+        <View className="items-center justify-center gap-5">
+          <Text className="text-4xl font-bold text-center">
             Ready to make a new friend?
           </Text>
-          <Text className="text-center font-regular text-xl">
+          <Text className="text-xl text-center font-regular">
             Let's adopt a pet you love and make your life happy.
           </Text>
         </View>
-        <Link
-          href="/"
-          className="bg-primary text-center font-bold text-xl rounded-lg p-5"
-        >
-          Get Started
-        </Link>
+        <Pressable onPress={onPress} className="p-5 rounded-lg bg-primary">
+          <Text className="text-xl font-bold text-center">Get Started</Text>
+        </Pressable>
       </View>
     </View>
   );
